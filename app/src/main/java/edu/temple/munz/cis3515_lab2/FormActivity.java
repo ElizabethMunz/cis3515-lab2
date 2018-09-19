09e4db6e97ebbfacfd806a584abe762f2d637092
ElizabethMunz <munz@temple.edu>
2018-09-19 13:56:28 -0400

    implemented check for password min length and email format

--- app/src/main/java/edu/temple/munz/cis3515_lab2/FormActivity.java
+++ app/src/main/java/edu/temple/munz/cis3515_lab2/FormActivity.java
@@ -9,30 +9,35 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.Normalizer;
+
 public class FormActivity extends AppCompatActivity {
 
 
-    TextView nameInput, emailInput, passwordInput, passwordInput2;
+    TextView nameInput, emailInput, passwordInput, passwordInput2, passwordInfo;
     Button saveButton;
 
-    public final String ERROR_EMPTY_FIELD = "Please fill out all fields.";
-    public final String ERROR_PASSWORD_MISMATCH = "Passwords must match.";
-    public final int REQUEST_CODE = 117;
+    private final String ERROR_EMPTY_FIELD = "Please fill out all fields.";
+    private final String ERROR_PASSWORD_MISMATCH = "Passwords must match.";
+    private final String ERROR_EMAIL_FORMAT = "Must be a valid email address format.";
+    private final int REQUEST_CODE = 117;
+    private final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private final int PASSWORD_MIN_LENGTH = 8;
 
-    public final String NAME_KEY = "nmk";
+    private final String NAME_KEY = "nmk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        //initialize the 4 text fields & button
+        //initialize the text fields & button
         nameInput = findViewById(R.id.nameInput);
         emailInput = findViewById(R.id.emailInput);
         passwordInput = findViewById(R.id.passwordInput);
         passwordInput2 = findViewById(R.id.passwordInput2);
         saveButton = findViewById(R.id.saveButton);
-
+        passwordInfo = findViewById(R.id.passwordInfo);
 
         //define onClick activity for Save button
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -48,12 +53,18 @@ public class FormActivity extends AppCompatActivity {
                 if (name.isEmpty() || email.isEmpty() || password.isEmpty() || password2.isEmpty()) {
                     Toast.makeText(FormActivity.this, ERROR_EMPTY_FIELD, Toast.LENGTH_LONG).show();
                 }
+                //check email format
+                else if (!email.matches(EMAIL_PATTERN)) {
+                    Toast.makeText(FormActivity.this, ERROR_EMAIL_FORMAT, Toast.LENGTH_LONG).show();
+                }
                 //check if passwords match
                 else if(!password.equals(password2)) {
                     Toast.makeText(FormActivity.this, ERROR_PASSWORD_MISMATCH, Toast.LENGTH_LONG).show();
                 }
-                //TODO: implement password length requirements
-                //TODO: make sure email address is in correct form (regex)
+                //check password length
+                else if (password.length() < PASSWORD_MIN_LENGTH) {
+                    Toast.makeText(FormActivity.this, R.string.pass_info, Toast.LENGTH_LONG).show();
+                }
                 //user input is good, send it to the welcome page
                 else {
                     //create Intent
@@ -63,19 +74,15 @@ public class FormActivity extends AppCompatActivity {
                     //start welcomeActivity
                     startActivityForResult(startActivityIntent, REQUEST_CODE);
                 }
-
             }
         });
 
+        //give user a popup warning of password length requirements
         passwordInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            String info = "Passwords must be at least 8 characters long.";
             @Override
             public void onFocusChange(View view, boolean hasFocus) {
-                if(hasFocus) {
-                    Toast passwordInfo = Toast.makeText(FormActivity.this, info, Toast.LENGTH_LONG);
-                    passwordInfo.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
-                    passwordInfo.show();
-                }
+                //make password length popup appear when you click onto password input, disappear when you click off
+                passwordInfo.setVisibility((hasFocus) ? View.VISIBLE : View.INVISIBLE);
             }
         });
 
