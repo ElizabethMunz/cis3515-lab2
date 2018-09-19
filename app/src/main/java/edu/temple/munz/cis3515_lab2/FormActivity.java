8ac6a30c7aa86bef748e6f91780c8bd5dfa32d36
ElizabethMunz <munz@temple.edu>
2018-09-19 09:54:19 -0400

    implemented saveButton to check for empty fields and password matches

--- app/src/main/java/edu/temple/munz/cis3515_lab2/FormActivity.java
+++ app/src/main/java/edu/temple/munz/cis3515_lab2/FormActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
 
     TextView nameInput, emailInput, passwordInput, passwordInput2;
     Button saveButton;
+    String ERROR_EMPTY_FIELD = "Please fill out all fields.";
+    String ERROR_PASSWORD_MISMATCH = "Passwords must match.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,25 @@ public class FormActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //get the user data from the form
+                String name = nameInput.getText().toString();
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                String password2 = passwordInput2.getText().toString();
+
+                //check if any fields are blank
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty() || password2.isEmpty()) {
+                    Toast.makeText(FormActivity.this, ERROR_EMPTY_FIELD, Toast.LENGTH_LONG).show();
+                }
+                //check if passwords match
+                else if(!password.equals(password2)) {
+                    Toast.makeText(FormActivity.this, ERROR_PASSWORD_MISMATCH, Toast.LENGTH_LONG).show();
+                }
+                //user input is good, send it to the welcome page
+                else {
+
+                }
+
 
             }
         });
---
+++ app/src/main/java/edu/temple/munz/cis3515_lab2/welcomeActivity.java
@@ -0,0 +1,13 @@
+package edu.temple.munz.cis3515_lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
